c2822be80142ac38e271e58f695cec9e07e3a021
gui352 <lintzguigui1323@gmail.com>
2022-02-02 11:40:57 -0300

    02022022

--- src/main/resources/db/migration/V0002__createTableChamada.sql
+++ src/main/resources/db/migration/V0002__createTableChamada.sql
@@ -5,6 +5,3 @@ CREATE TABLE chamada (
     presenca boolean not null,
     primary key(id_frequencia)
 );
-
-ALTER TABLE chamada ADD CONSTRAINT fk_alunos_chamada
-FOREIGN KEY (id_Aluno) REFERENCES alunos (n_Cadastro);
